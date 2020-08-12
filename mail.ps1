6a648557f75ac7cce7da6463e19bcb59515a2a1e
saridjapeter <68054303+saridjapeter@users.noreply.github.com>
2020-08-12 16:25:43 +0300

    добавлено вложение файла в письмо

--- mail.ps1
+++ mail.ps1
@@ -1,16 +1,31 @@
 ﻿#Install-Module -Name SqlServer
 #Install-Module -Name SqlServer -Scope CurrentUser
-
-#$ins = "192.168.0.11"
-#$user="sa"
-#$pass="pCOc6Ly79p"
+#$Path = $PSScriptRoot
+$Path = "c:\mail\"
+#$relativePath = ((Get-Content ("config.ini"))[3] -replace ".*=") | Resolve-Path -Relative
+$ins = (Get-Content ($Path+ "config.ini"))[0] -replace ".*="
+$login=(Get-Content ($Path+ "config.ini"))[1] -replace ".*="
+$password=(Get-Content ($Path+ "config.ini"))[2] -replace ".*="
 #Start-Transcript -Path ("D:\mail\"+(Get-Date -Format d)+".log")
 while($true) {
 #Смотрим письмо в Email
-$query=Invoke-Sqlcmd -ServerInstance 192.168.0.11 -Username sa -Password pCOc6Ly79p -Database Claim "set nocount on;SELECT TOP (1) Receiver, Message_Text, Subject, ID_Attachment, ID_Source FROM Email"
+$query=Invoke-Sqlcmd -ServerInstance $ins -Username $login -Password $password -Database Claim "set nocount on;SELECT TOP (1) Receiver, Message_Text, Subject, ID_Attachment, ID_Source FROM Email"
 if ($query) {
+if ($query[3]){
+$dir = Get-ChildItem -Path C:\ClaimDesk\Attach -Include $query[3]+".jpg" -Recurse -Force -ErrorAction SilentlyContinue
+
+#Путь до файла 
+$file = $dir.DirectoryName+$query[3]+".jpg"
+
+#Создаем экземпляра класса
+$att = New-object Net.Mail.Attachment($file)
+
+#Добавляем файл
+$mes.Attachments.Add($att) 
+}
+
 #Берем настройки с dbo.Config
-$config=Invoke-Sqlcmd -ServerInstance 192.168.0.11 -Username sa -Password pCOc6Ly79p -Database Claim "set nocount on;SELECT Data FROM Config"
+$config=Invoke-Sqlcmd -ServerInstance $ins -Username $login -Password $password -Database Claim "set nocount on;SELECT Data FROM Config"
 
 #Адрес сервера SMTP для отправки
 $serverSmtp = $config[6].Data 
@@ -31,7 +46,7 @@ $subject = $query[2]
 $user = $config[7].Data 
 $pass = $config[8].Data 
 
-#Создаем два экземпляра класса
+#Создаем экземпляра класса
 $mes = New-Object System.Net.Mail.MailMessage
 
 #Формируем данные для отправки
@@ -54,15 +69,15 @@ $smtp.Credentials = New-Object System.Net.NetworkCredential($user, $pass);
 $smtp.Send($mes) 
 
 if ($?) { # Если SMTP сообщение было отправлено успешно, то
-        write-host "Сообщение успешно отправлено"
-        Add-content ("D:\mail\"+(Get-Date -Format d)+".log") (((Get-Date -Format u).Substring(0,(Get-Date -Format u).Length -1))+" Сообщение успешно отправлено")
+        #write-host "Сообщение успешно отправлено"
+        Add-content ($Path+ "\log\"+(Get-Date -Format d)+".log") (((Get-Date -Format u).Substring(0,(Get-Date -Format u).Length -1))+" Сообщение успешно отправлено")
         #Добавляем в Dispatch
-        Invoke-Sqlcmd -ServerInstance 192.168.0.11 -Username sa -Password pCOc6Ly79p -Database Claim "INSERT INTO Dispatch (Receiver, Message, ID_Source, ID_Attach, Message_Type, Message_Time) SELECT TOP (1) Receiver, Message_Text, ID_Source, ID_Attachment, Message_Type='0', Message_Time=GETDATE()  FROM Email"  
+        Invoke-Sqlcmd -ServerInstance $ins -Username $login -Password $password -Database Claim "INSERT INTO Dispatch (Receiver, Message, ID_Source, ID_Attach, Message_Type, Message_Time) SELECT TOP (1) Receiver, Message_Text, ID_Source, ID_Attachment, Message_Type='0', Message_Time=GETDATE()  FROM Email"  
         #Удаляем запись в Email
-        Invoke-Sqlcmd -ServerInstance 192.168.0.11 -Username sa -Password pCOc6Ly79p -Database Claim "DELETE TOP(1) FROM Email"
+        Invoke-Sqlcmd -ServerInstance $ins -Username $login -Password $password -Database Claim "DELETE TOP(1) FROM Email"
     } else { #Если SMTP сообщение не удалось отправить, то 
-        write-host "Сообщение не отправлено. Ошибка:" $Error[0].ToString()
-        Add-content ("D:\mail\"+(Get-Date -Format d)+".log") (((Get-Date -Format u).Substring(0,(Get-Date -Format u).Length -1))+ $Error[0].ToString())
+        #write-host "Сообщение не отправлено. Ошибка:" $Error[0].ToString()
+        Add-content ($Path+ "\log\"+(Get-Date -Format d)+".log") (((Get-Date -Format u).Substring(0,(Get-Date -Format u).Length -1))+ $Error[0].ToString())
     }
 
 #Отправляем сообщение QUIT на SMTP-сервер (правильно завершаем TCP-подключение и освобождаем все ресурсы, используемые текущим экземпляром класса SmtpClient)
---
+++ service install.ps1
@@ -0,0 +1,14 @@
+﻿#Set-ExecutionPolicy RemoteSigned
+$NSSMPath = ($PSScriptRoot+ "\nssm.exe")
+$NewServiceName = “ClaimDeskMailSend”
+$PoShPath= (Get-Command powershell).Source
+$PoShScriptPath = ($PSScriptRoot+ “\mail.ps1”)
+$args = '-ExecutionPolicy Bypass -NoProfile -File "{0}"' -f $PoShScriptPath
+& $NSSMPath install $NewServiceName $PoShPath $args
+& $NSSMPath status $NewServiceName
+
+Start-Service $NewServiceName
+
+Get-Service $NewServiceName
+
+#(Get-WmiObject win32_service -Filter "name='ClaimDeskMailSend'").delete()
